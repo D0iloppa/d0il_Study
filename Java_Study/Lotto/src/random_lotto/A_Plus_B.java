18e85088f83547fc799cd2f3f7b391d63688b9be
Dovian maier <44669890+D0iloppa@users.noreply.github.com>
2021-03-17 10:27:12 +0900

    change

---
+++ Java_Study/Lotto/src/random_lotto/A_Plus_B.java
@@ -0,0 +1,15 @@
+package random_lotto;
+
+import java.util.Scanner;
+
+public class A_Plus_B {
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a+b);
+		
+	}
+
+}
