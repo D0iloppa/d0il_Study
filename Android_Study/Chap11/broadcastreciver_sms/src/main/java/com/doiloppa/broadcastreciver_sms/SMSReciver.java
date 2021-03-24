41daeec984f41325e7d60bb4a540dcde983249de
백제 <kdi3939@gmail.com>
2021-03-24 11:33:52 +0900

    2021-03-24

--- Android_Study/Chap11/broadcastreciver_sms/src/main/java/com/doiloppa/broadcastreciver_sms/SMSReciver.java
+++ Android_Study/Chap11/broadcastreciver_sms/src/main/java/com/doiloppa/broadcastreciver_sms/SMSReciver.java
@@ -3,14 +3,60 @@ package com.doiloppa.broadcastreciver_sms;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SMSReciver extends BroadcastReceiver {
 
+    // 시간이 표시되는 포맷정의 (디폴트는 미국식이라서 헷갈릴 수 있기때문에 보기 편한 형태로 정의해줌)
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        // 문자메시지를 받았을 때 처리하는 이벤트
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] message = parseSMSMessage(bundle);
+
+        // 제일 최근 메시지의 데이터들을 각각 변수에 담아서 intent에 put해준다.
+
+        String sender = message[0].getOriginatingAddress(); // 발신번호 확인
+        String content = message[0].getMessageBody(); // 발신 메시지 확인
+        Date receiveDate = new Date(message[0].getTimestampMillis()); // 수신시간 확인
+
+        intent = new Intent(context, SmsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("sender",sender);
+        intent.putExtra("contents",content);
+        intent.putExtra("receiveDate",dateFormat.format(receiveDate)); // 시간을 포맷에 맞게 변환시키고 추가
+        // 여기까지 백엔드에서 처리되는 부분
+
+        // 액티비티를 실행시켜준다.
+        context.startActivity(intent);
+
+    }
+
+    private SmsMessage[] parseSMSMessage(Bundle bundle){
+        // sms 데이터를 처리하는 SMTP가 있는데
+        // 그 안에 pdus라는 이름의 sms 정보들이 들어있음
+        // 실제 메시지는 pdus를 object[]로 변환해야 함
+        Object[] objects = (Object[])bundle.get("pdus");
+        // sms를 받아올 SMS Message 배열을 만듬
+        SmsMessage[] messages = new SmsMessage[objects.length];
+        for(int i=0;i<objects.length;i++){
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                // 각 메시지를 추출하기 위해 사용( Pdu 포맷으로 되어있는 메시지를 복원
+                String format = bundle.getString("format");
+                messages[i] = SmsMessage.createFromPdu((byte[])objects[i],format);
+            } else
+                messages[i] = SmsMessage.createFromPdu((byte[])objects[i]);
+        }
+
+        return messages;
     }
 
 }
--- Android_Study/Chap11/broadcastreciver_sms/src/main/java/com/doiloppa/broadcastreciver_sms/SmsActivity.java
+++ Android_Study/Chap11/broadcastreciver_sms/src/main/java/com/doiloppa/broadcastreciver_sms/SmsActivity.java
@@ -2,13 +2,60 @@ package com.doiloppa.broadcastreciver_sms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.gesture.GestureOverlayView;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SmsActivity extends AppCompatActivity {
+
+    EditText edt_Sender, edt_Contents, edt_Date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
+
+        edt_Sender = findViewById(R.id.editText);
+        edt_Contents = findViewById(R.id.editText2);
+        edt_Date = findViewById(R.id.editText3);
+        Button button = findViewById(R.id.button);
+
+        Intent intent = getIntent();
+        processIntent(intent);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
     }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        processIntent(intent);
+    }
+
+    private void processIntent(Intent intent) {
+
+        if (intent != null) {
+            String sender = intent.getStringExtra("sender");
+            String content = intent.getStringExtra("contents");
+            String receiveDate = intent.getStringExtra("receiveDate");
+
+            edt_Sender.setText(sender);
+            edt_Contents.setText(content);
+            edt_Date.setText(receiveDate);
+        }
+
+    }
+
+
 }
