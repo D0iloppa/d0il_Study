9608d5310006ec7da824e3e7fcdf6c5602cc7405
백제 <kdi3939@gmail.com>
2021-04-02 15:06:46 +0900

    cc

--- Android_Study/Chap11/preferencefragment_ex/src/main/java/com/doiloppa/preferencefragment_ex/MainActivity.java
+++ Android_Study/Chap11/preferencefragment_ex/src/main/java/com/doiloppa/preferencefragment_ex/MainActivity.java
@@ -1,7 +1,9 @@
 package com.doiloppa.preferencefragment_ex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SettingPreferenceFragment settingPreferenceFragment = new SettingPreferenceFragment();
+        Intent intent = getIntent();
+        if(intent!=null){
+            String rootKey = intent.getStringExtra("target");
+            if(rootKey!=null){
+                Bundle bundle = new Bundle();
+                bundle.putString(PreferenceFragmentCompat.ARG_PREFERENCE_ROOT, rootKey);
+                settingPreferenceFragment.setArguments(bundle);
+            }
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, settingPreferenceFragment,null).commit();
+
+
     }
 }
--- Android_Study/Chap11/preferencefragment_ex/src/main/java/com/doiloppa/preferencefragment_ex/SettingPreferenceFragment.java
+++ Android_Study/Chap11/preferencefragment_ex/src/main/java/com/doiloppa/preferencefragment_ex/SettingPreferenceFragment.java
@@ -1,11 +1,13 @@
 package com.doiloppa.preferencefragment_ex;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
 
 public class SettingPreferenceFragment extends PreferenceFragmentCompat {
 
@@ -16,29 +18,29 @@ public class SettingPreferenceFragment extends PreferenceFragmentCompat {
     SharedPreferences prfs;
 
 
-
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.settings_preference,rootKey);
+        setPreferencesFromResource(R.xml.settings_preference, rootKey);
+        if (rootKey == null) {
+            soundPreference = findPreference("sound_List");
+            keywordScreen = findPreference("keyword_screen");
+            editPreference = findPreference("nickname");
 
-        soundPreference = findPreference("sound_List");
-        keywordScreen = findPreference("keyword_screen");
-        editPreference = findPreference("nickname");
+            prfs = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
-        prfs = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
-        if(!prfs.getString("sound_List","").equals(""))
-            soundPreference.setSummary(prfs.getString("sound_List","카톡"));
+            if (!prfs.getString("sound_List", "").equals(""))
+                soundPreference.setSummary(prfs.getString("sound_List", "카톡"));
 
-        if(prfs.getBoolean("keyword",false))
-            keywordScreen.setSummary("사용");
-        else keywordScreen.setSummary("사용안함");
+            if (prfs.getBoolean("keyword", false))
+                keywordScreen.setSummary("사용");
+            else keywordScreen.setSummary("사용안함");
 
-        if(!prfs.getString("nickname","").equals("")){
-            editPreference.setSummary(prfs.getString("nickname","닉네임을 설정해주세요"));
+            if (!prfs.getString("nickname", "").equals(""))
+                editPreference.setSummary(prfs.getString("nickname", "닉네임을 설정해주세요"));
+
             prfs.registerOnSharedPreferenceChangeListener(preferenceChangeListener);
         }
-
 
 
     }
@@ -46,16 +48,24 @@ public class SettingPreferenceFragment extends PreferenceFragmentCompat {
     SharedPreferences.OnSharedPreferenceChangeListener preferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            if(key.equals("sound_List"))
-                soundPreference.setSummary(prfs.getString("sound_List","카톡"));
+            if (key.equals("sound_List"))
+                soundPreference.setSummary(prfs.getString("sound_List", "카톡"));
 
-            if(prfs.getBoolean("keyword",false))
+            if (prfs.getBoolean("keyword", false))
                 keywordScreen.setSummary("사용");
             else keywordScreen.setSummary("사용안함");
 
-            if(key.equals("nickname"))
-                editPreference.setSummary(prfs.getString("nickname","닉네임을 설정해주세요"));
+            if (key.equals("nickname"))
+                editPreference.setSummary(prfs.getString("nickname", "닉네임을 설정해주세요"));
         }
     };
+
+    @Override
+    public void onNavigateToScreen(PreferenceScreen preferenceScreen) {
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        intent.putExtra("target", preferenceScreen.getKey());
+        startActivity(intent);
+    }
+
 
 }
