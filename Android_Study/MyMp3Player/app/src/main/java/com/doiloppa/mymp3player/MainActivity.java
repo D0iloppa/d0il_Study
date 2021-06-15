d09efabff32b7c19afd5e4dcd2b05478e2c53ce9
Dovian maier <44669890+D0iloppa@users.noreply.github.com>
2021-06-15 14:46:13 +0900

    3차 평가 다듬기

--- Android_Study/MyMp3Player/app/src/main/java/com/doiloppa/mymp3player/MainActivity.java
+++ Android_Study/MyMp3Player/app/src/main/java/com/doiloppa/mymp3player/MainActivity.java
@@ -27,8 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ArrayList<String> list;
 
 
-    Button btn_Play,btn_Pause,btn_Stop;
-    TextView tv_Title,tv_Time;
+    Button btn_Play, btn_Pause, btn_Stop;
+    TextView tv_Title, tv_Time;
     SeekBar seekBar;
     String sdPath;
     String uri;
@@ -36,25 +36,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     MediaPlayer mediaPlayer;
     boolean isPlayin = false;
     int list_num = -1;
-    int pos=0;
+    int pos = 0; // 음악의 현재 위치
 
-    public String getPlayTime(int sec){
-        int m,s;
+    public String getPlayTime(int sec) {
+        int m, s;
         s = sec % 60;
         m = sec / 60;
-        return String.format("%02d:%02d",m,s);
+        return String.format("%02d:%02d", m, s);
 
     }
+
     // 음악 재생을 위한 스레드
-    class PlayThread extends Thread{
+    class PlayThread extends Thread {
         @Override
         public void run() {
-            tv_Title.setText("실행중인 음악 : "+list.get(list_num));
+            tv_Title.setText("실행중인 음악 : " + list.get(list_num));
 
 
-            while(isPlayin){
+            while (isPlayin) {
                 seekBar.setProgress(mediaPlayer.getCurrentPosition());
-                tv_Time.setText("진행 시간 : " + getPlayTime(seekBar.getProgress()/1000));
+                tv_Time.setText("진행 시간 : " + getPlayTime(seekBar.getProgress() / 1000));
             }
 
         }
@@ -66,14 +67,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions(this,new String[]{
+        ActivityCompat.requestPermissions(this, new String[]{
                 "android.permission.WRITE_EXTERNAL_STORAGE",
                 "android.permission.READ_EXTERNAL_STORAGE"
-        },0);
+        }, 0);
 
         listView = findViewById(R.id.music_list);
         list = new ArrayList<>();
-        adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_single_choice,list);
+        adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_single_choice, list);
         listView.setAdapter(adapter);
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
@@ -84,44 +85,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String selectedItem;
                 selectedItem = list.get(position);
                 list_num = position;
-                uri = sdPath+"/"+selectedItem;
+                uri = sdPath + "/" + selectedItem;
             }
         });
-
-
-
 
 
         //////////////////////////////
         // SD카드 파일 불러오기
 
-        String ext = Environment.getExternalStorageState();
+        String externalStorageState = Environment.getExternalStorageState();
 
-        if(ext.equals(Environment.MEDIA_MOUNTED))
+        if (externalStorageState.equals(Environment.MEDIA_MOUNTED))
             sdPath = Environment.getExternalStorageDirectory().getAbsolutePath();
         else sdPath = Environment.MEDIA_UNMOUNTED;
 
+        try {
+            File sdFile = new File(sdPath);
 
+            String[] filenames = sdFile.list(null);
 
-        try{
-            File f = new File(sdPath);
-
-            String[] filenames = f.list(null);
-
-            for (int i = 0; i < filenames.length ; i++){
+            for (int i = 0; i < filenames.length; i++) {
                 String filename = filenames[i];
+                // mp3로 끝나는 파일만 가져와서 리스트에 추가
                 if (filename.toLowerCase().endsWith("mp3")) {
-                    String result = filename;
-                    list.add(result);
+                    String mp3File = filename;
+                    list.add(mp3File);
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "파일 리스트 불러오기 실패", Toast.LENGTH_SHORT).show();
 
         }
 
-        //////////////////////
-        // 나머지 뷰 가져오기
+        // 뷰 가져오기
         btn_Play = findViewById(R.id.btn_Start);
         btn_Play.setOnClickListener(this);
         btn_Pause = findViewById(R.id.btn_Pause);
@@ -138,7 +134,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if(seekBar.getMax()==progress){
+                if (seekBar.getMax() == progress) {
                     // 프로그레스가 가득 찼을 경우
                     isPlayin = false;
                     mediaPlayer.stop();
@@ -165,24 +161,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
 
-
-
-
-
-
     }
 
     @Override
     public void onClick(View v) {
 
-        switch(v.getId()){
-            case R.id.btn_Start : // 재생버튼
+        switch (v.getId()) {
+            case R.id.btn_Start: // 재생버튼
                 play();
                 break;
-            case R.id.btn_Pause : // 일시정지 버튼
+            case R.id.btn_Pause: // 일시정지 버튼
                 pause();
                 break;
-            case R.id.btn_Stop : // 중지 버튼
+            case R.id.btn_Stop: // 중지 버튼
                 stop();
                 break;
         }
@@ -190,22 +181,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-
     private void play() {
-        if(list_num==-1){
+        if (list_num == -1) {
+            // 아직 리스트가 체크되지 않았으므로 재생시키지 않는다.
             Toast.makeText(getApplicationContext(), "먼저 파일을 선택해주세요", Toast.LENGTH_SHORT).show();
             return;
         }
-        if(!isPlayin){ // 재생중이 아닐때만 작동되도록
-            if(mediaPlayer==null) {
+        if (!isPlayin) { // 재생중이 아닐때만 작동되도록한다. 재생중에 또 재생 누르면 아무 반응x
+            if (mediaPlayer == null) {
                 mediaPlayer = new MediaPlayer();
                 try {
                     mediaPlayer.setDataSource(uri);
                     mediaPlayer.prepare();
                     mediaPlayer.start();
 
-                    int a = mediaPlayer.getDuration(); // 노래의 재생시간
-                    seekBar.setMax(a);// seekbar의 최대크기
+                    int music_Length = mediaPlayer.getDuration(); // 노래의 길이
+                    seekBar.setMax(music_Length);// seekbar의 최대크기를 노래의 길이로 지정
                     PlayThread playThread = new PlayThread(); //
                     playThread.start();
                     isPlayin = true;
@@ -213,7 +204,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }else{
+            } else {
                 mediaPlayer.seekTo(pos);
                 mediaPlayer.start();
                 isPlayin = true;
@@ -224,26 +215,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void pause() {
+        if (mediaPlayer == null) return;
+
         pos = mediaPlayer.getCurrentPosition();
         mediaPlayer.pause();
         isPlayin = false;
     }
 
     private void stop() {
-        isPlayin = false;
+        if (mediaPlayer == null) return;
 
-        if(mediaPlayer==null) return;
+        isPlayin = false;
         mediaPlayer.stop();
         mediaPlayer.release();
         mediaPlayer = null;
 
+        // 초기화
         seekBar.setProgress(0);
         pos = 0;
 
         tv_Title.setText("실행중인 음악 :");
         tv_Time.setText("진행 시간 :");
     }
-
 
 
 }
